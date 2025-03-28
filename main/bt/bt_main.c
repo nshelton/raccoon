9146fa0ebbadb343a9182e8e1a2819aa8d1599e7
Nicholas Shelton <nick@illuminant.ai>
2025-03-28 00:28:29 -0700

    put bluetooth in there

---
+++ main/bt/bt_main.c
@@ -0,0 +1,171 @@
+/*
+ * SPDX-FileCopyrightText: 2021-2024 Espressif Systems (Shanghai) CO LTD
+ *
+ * SPDX-License-Identifier: Unlicense OR CC0-1.0
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+#include <inttypes.h>
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+#include "nvs.h"
+#include "nvs_flash.h"
+#include "esp_system.h"
+#include "esp_log.h"
+
+#include "esp_bt.h"
+#include "bt_app_core.h"
+#include "bt_app_av.h"
+#include "esp_bt_main.h"
+#include "esp_bt_device.h"
+#include "esp_gap_bt_api.h"
+#include "esp_a2dp_api.h"
+#include "esp_avrc_api.h"
+
+/* CONFIG STUFF */
+// #define EXAMPLE_I2S_DATA_PIN 26
+// #define EXAMPLE_I2S_BCK_PIN  33
+#define EXAMPLE_LOCAL_DEVICE_NAME "raccoon"
+#define EXAMPLE_A2DP_SINK_OUTPUT_INTERNAL_DAC
+// #define EXAMPLE_A2DP_SINK_OUTPUT_EXTERNAL_I2S
+
+
+
+/* device name */
+static const char local_device_name[] = EXAMPLE_LOCAL_DEVICE_NAME;
+
+/* event for stack up */
+enum {
+    BT_APP_EVT_STACK_UP = 0,
+};
+
+/********************************
+ * STATIC FUNCTION DECLARATIONS
+ *******************************/
+
+/* GAP callback function */
+static void bt_app_gap_cb(esp_bt_gap_cb_event_t event, esp_bt_gap_cb_param_t *param);
+/* handler for bluetooth stack enabled events */
+static void bt_av_hdl_stack_evt(uint16_t event, void *p_param);
+
+/*******************************
+ * STATIC FUNCTION DEFINITIONS
+ ******************************/
+static char *bda2str(uint8_t * bda, char *str, size_t size)
+{
+    if (bda == NULL || str == NULL || size < 18) {
+        return NULL;
+    }
+
+    uint8_t *p = bda;
+    sprintf(str, "%02x:%02x:%02x:%02x:%02x:%02x",
+            p[0], p[1], p[2], p[3], p[4], p[5]);
+    return str;
+}
+
+static void bt_app_gap_cb(esp_bt_gap_cb_event_t event, esp_bt_gap_cb_param_t *param)
+{
+    uint8_t *bda = NULL;
+
+    switch (event) {
+    /* when authentication completed, this event comes */
+    case ESP_BT_GAP_AUTH_CMPL_EVT: {
+        if (param->auth_cmpl.stat == ESP_BT_STATUS_SUCCESS) {
+            ESP_LOGI(BT_AV_TAG, "authentication success: %s", param->auth_cmpl.device_name);
+            esp_log_buffer_hex(BT_AV_TAG, param->auth_cmpl.bda, ESP_BD_ADDR_LEN);
+        } else {
+            ESP_LOGE(BT_AV_TAG, "authentication failed, status: %d", param->auth_cmpl.stat);
+        }
+        ESP_LOGI(BT_AV_TAG, "link key type of current link is: %d", param->auth_cmpl.lk_type);
+        break;
+    }
+    case ESP_BT_GAP_ENC_CHG_EVT: {
+        char *str_enc[3] = {"OFF", "E0", "AES"};
+        bda = (uint8_t *)param->enc_chg.bda;
+        ESP_LOGI(BT_AV_TAG, "Encryption mode to [%02x:%02x:%02x:%02x:%02x:%02x] changed to %s",
+                 bda[0], bda[1], bda[2], bda[3], bda[4], bda[5], str_enc[param->enc_chg.enc_mode]);
+        break;
+    }
+
+#if (CONFIG_EXAMPLE_A2DP_SINK_SSP_ENABLED == true)
+    /* when Security Simple Pairing user confirmation requested, this event comes */
+    case ESP_BT_GAP_CFM_REQ_EVT:
+        ESP_LOGI(BT_AV_TAG, "ESP_BT_GAP_CFM_REQ_EVT Please compare the numeric value: %"PRIu32, param->cfm_req.num_val);
+        esp_bt_gap_ssp_confirm_reply(param->cfm_req.bda, true);
+        break;
+    /* when Security Simple Pairing passkey notified, this event comes */
+    case ESP_BT_GAP_KEY_NOTIF_EVT:
+        ESP_LOGI(BT_AV_TAG, "ESP_BT_GAP_KEY_NOTIF_EVT passkey: %"PRIu32, param->key_notif.passkey);
+        break;
+    /* when Security Simple Pairing passkey requested, this event comes */
+    case ESP_BT_GAP_KEY_REQ_EVT:
+        ESP_LOGI(BT_AV_TAG, "ESP_BT_GAP_KEY_REQ_EVT Please enter passkey!");
+        break;
+#endif
+
+    /* when GAP mode changed, this event comes */
+    case ESP_BT_GAP_MODE_CHG_EVT:
+        ESP_LOGI(BT_AV_TAG, "ESP_BT_GAP_MODE_CHG_EVT mode: %d, interval: %.2f ms",
+                 param->mode_chg.mode, param->mode_chg.interval * 0.625);
+        break;
+    /* when ACL connection completed, this event comes */
+    case ESP_BT_GAP_ACL_CONN_CMPL_STAT_EVT:
+        bda = (uint8_t *)param->acl_conn_cmpl_stat.bda;
+        ESP_LOGI(BT_AV_TAG, "ESP_BT_GAP_ACL_CONN_CMPL_STAT_EVT Connected to [%02x:%02x:%02x:%02x:%02x:%02x], status: 0x%x",
+                 bda[0], bda[1], bda[2], bda[3], bda[4], bda[5], param->acl_conn_cmpl_stat.stat);
+        break;
+    /* when ACL disconnection completed, this event comes */
+    case ESP_BT_GAP_ACL_DISCONN_CMPL_STAT_EVT:
+        bda = (uint8_t *)param->acl_disconn_cmpl_stat.bda;
+        ESP_LOGI(BT_AV_TAG, "ESP_BT_GAP_ACL_DISC_CMPL_STAT_EVT Disconnected from [%02x:%02x:%02x:%02x:%02x:%02x], reason: 0x%x",
+                 bda[0], bda[1], bda[2], bda[3], bda[4], bda[5], param->acl_disconn_cmpl_stat.reason);
+        break;
+    /* others */
+    default: {
+        ESP_LOGI(BT_AV_TAG, "event: %d", event);
+        break;
+    }
+    }
+}
+
+static void bt_av_hdl_stack_evt(uint16_t event, void *p_param)
+{
+    ESP_LOGD(BT_AV_TAG, "%s event: %d", __func__, event);
+
+    switch (event) {
+    /* when do the stack up, this event comes */
+    case BT_APP_EVT_STACK_UP: {
+        esp_bt_gap_set_device_name(local_device_name);
+        esp_bt_gap_register_callback(bt_app_gap_cb);
+
+        assert(esp_avrc_ct_init() == ESP_OK);
+        esp_avrc_ct_register_callback(bt_app_rc_ct_cb);
+        assert(esp_avrc_tg_init() == ESP_OK);
+        esp_avrc_tg_register_callback(bt_app_rc_tg_cb);
+
+        esp_avrc_rn_evt_cap_mask_t evt_set = {0};
+        esp_avrc_rn_evt_bit_mask_operation(ESP_AVRC_BIT_MASK_OP_SET, &evt_set, ESP_AVRC_RN_VOLUME_CHANGE);
+        assert(esp_avrc_tg_set_rn_evt_cap(&evt_set) == ESP_OK);
+
+        assert(esp_a2d_sink_init() == ESP_OK);
+        esp_a2d_register_callback(&bt_app_a2d_cb);
+        esp_a2d_sink_register_data_callback(bt_app_a2d_data_cb);
+
+        /* Get the default value of the delay value */
+        esp_a2d_sink_get_delay_value();
+        /* Get local device name */
+        esp_bt_gap_get_device_name();
+
+        /* set discoverable and connectable mode, wait to be connected */
+        esp_bt_gap_set_scan_mode(ESP_BT_CONNECTABLE, ESP_BT_GENERAL_DISCOVERABLE);
+        break;
+    }
+    /* others */
+    default:
+        ESP_LOGE(BT_AV_TAG, "%s unhandled event: %d", __func__, event);
+        break;
+    }
+}
--- main/main.c
+++ main/main.c
@@ -1,67 +1,163 @@
+// /*
+//  * SPDX-FileCopyrightText: 2021-2024 Espressif Systems (Shanghai) CO LTD
+//  *
+//  * SPDX-License-Identifier: CC0-1.0
+//  */
+
+// #include <stdio.h>
+// #include "freertos/FreeRTOS.h"
+// #include "freertos/task.h"
+// #include "esp_log.h"
+// #include "lcd.h" // Include the LCD header
+// #include "esp_timer.h"
+
+
+// static const char *TAG = "example";
+
+// void app_main(void)
+// {
+//     ESP_LOGI(TAG, "Initializing LCD");
+    
+//     // Initialize LCD
+//     lcd_handle_t lcd = lcd_init();
+//     if (!lcd) {
+//         ESP_LOGE(TAG, "Failed to initialize LCD");
+//         return;
+//     }
+    
+//     ESP_LOGI(TAG, "Display initialized successfully");
+
+//     int64_t last_us = 0;
+//     int64_t last_display_us = 0;
+    
+//     while (1) {
+//         // Clear buffer before each redraw
+//         lcd_clear_buffer(lcd);
+        
+//         // Draw text with mixed case
+        
+//         // Get current time and display it
+//         char ms[32];
+//         uint32_t time_ms = esp_timer_get_time();
+
+//         snprintf(ms, sizeof(ms), "%lu", time_ms);
+//         lcd_draw_string(lcd, ms, 1, 0);
+        
+//         int64_t diff = time_ms - last_us;
+//         snprintf(ms, sizeof(ms), "%lld dt", diff);
+//         lcd_draw_string(lcd, ms, 1, 10);
+//         last_us = time_ms;
+
+//         snprintf(ms, sizeof(ms), "%lldus display", last_display_us);
+//         lcd_draw_string(lcd, ms, 1, 20);
+
+//         uint32_t start = esp_timer_get_time();
+//         lcd_update(lcd);
+//         uint32_t end = esp_timer_get_time();
+//         last_display_us = end - start;
+        
+
+//         // IMPORTANT: Yield control to other tasks to prevent watchdog timer errors
+//         vTaskDelay(1);
+//     }
+    
+//     // In a real application, we would call lcd_deinit(lcd) when done using the LCD
+//     // lcd_deinit(lcd);
+// }
+   
+
 /*
  * SPDX-FileCopyrightText: 2021-2024 Espressif Systems (Shanghai) CO LTD
  *
- * SPDX-License-Identifier: CC0-1.0
+ * SPDX-License-Identifier: Unlicense OR CC0-1.0
  */
 
 #include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+#include <inttypes.h>
 #include "freertos/FreeRTOS.h"
 #include "freertos/task.h"
+#include "nvs.h"
+#include "nvs_flash.h"
+#include "esp_system.h"
 #include "esp_log.h"
-#include "lcd.h" // Include the LCD header
-#include "esp_timer.h"
+
+#include "esp_bt.h"
+#include "bt/bt_app_core.h"
+#include "bt/bt_app_av.h"
+#include "bt/bt_main.c"
+#include "esp_bt_main.h"
+#include "esp_bt_device.h"
+#include "esp_gap_bt_api.h"
+#include "esp_a2dp_api.h"
+#include "esp_avrc_api.h"
 
 
-static const char *TAG = "example";
+/*******************************
+ * MAIN ENTRY POINT
+ ******************************/
 
 void app_main(void)
 {
-    ESP_LOGI(TAG, "Initializing LCD");
-    
-    // Initialize LCD
-    lcd_handle_t lcd = lcd_init();
-    if (!lcd) {
-        ESP_LOGE(TAG, "Failed to initialize LCD");
+    char bda_str[18] = {0};
+    /* initialize NVS — it is used to store PHY calibration data */
+    esp_err_t err = nvs_flash_init();
+    if (err == ESP_ERR_NVS_NO_FREE_PAGES || err == ESP_ERR_NVS_NEW_VERSION_FOUND) {
+        ESP_ERROR_CHECK(nvs_flash_erase());
+        err = nvs_flash_init();
+    }
+    ESP_ERROR_CHECK(err);
+
+    /*
+     * This example only uses the functions of Classical Bluetooth.
+     * So release the controller memory for Bluetooth Low Energy.
+     */
+    ESP_ERROR_CHECK(esp_bt_controller_mem_release(ESP_BT_MODE_BLE));
+
+    esp_bt_controller_config_t bt_cfg = BT_CONTROLLER_INIT_CONFIG_DEFAULT();
+    if ((err = esp_bt_controller_init(&bt_cfg)) != ESP_OK) {
+        ESP_LOGE(BT_AV_TAG, "%s initialize controller failed: %s", __func__, esp_err_to_name(err));
         return;
     }
-    
-    ESP_LOGI(TAG, "Display initialized successfully");
-
-    int64_t last_us = 0;
-    int64_t last_display_us = 0;
-    
-    while (1) {
-        // Clear buffer before each redraw
-        lcd_clear_buffer(lcd);
-        
-        // Draw text with mixed case
-        
-        // Get current time and display it
-        char ms[32];
-        uint32_t time_ms = esp_timer_get_time();
-
-        snprintf(ms, sizeof(ms), "%lu", time_ms);
-        lcd_draw_string(lcd, ms, 1, 0);
-        
-        int64_t diff = time_ms - last_us;
-        snprintf(ms, sizeof(ms), "%lld dt", diff);
-        lcd_draw_string(lcd, ms, 1, 10);
-        last_us = time_ms;
-
-        snprintf(ms, sizeof(ms), "%lldus display", last_display_us);
-        lcd_draw_string(lcd, ms, 1, 20);
-
-        uint32_t start = esp_timer_get_time();
-        lcd_update(lcd);
-        uint32_t end = esp_timer_get_time();
-        last_display_us = end - start;
-        
-
-        // IMPORTANT: Yield control to other tasks to prevent watchdog timer errors
-        vTaskDelay(1);
+    if ((err = esp_bt_controller_enable(ESP_BT_MODE_CLASSIC_BT)) != ESP_OK) {
+        ESP_LOGE(BT_AV_TAG, "%s enable controller failed: %s", __func__, esp_err_to_name(err));
+        return;
     }
-    
-    // In a real application, we would call lcd_deinit(lcd) when done using the LCD
-    // lcd_deinit(lcd);
+
+    esp_bluedroid_config_t bluedroid_cfg = BT_BLUEDROID_INIT_CONFIG_DEFAULT();
+#if (CONFIG_EXAMPLE_A2DP_SINK_SSP_ENABLED == false)
+    bluedroid_cfg.ssp_en = false;
+#endif
+    if ((err = esp_bluedroid_init_with_cfg(&bluedroid_cfg)) != ESP_OK) {
+        ESP_LOGE(BT_AV_TAG, "%s initialize bluedroid failed: %s", __func__, esp_err_to_name(err));
+        return;
+    }
+
+    if ((err = esp_bluedroid_enable()) != ESP_OK) {
+        ESP_LOGE(BT_AV_TAG, "%s enable bluedroid failed: %s", __func__, esp_err_to_name(err));
+        return;
+    }
+
+#if (CONFIG_EXAMPLE_A2DP_SINK_SSP_ENABLED == true)
+    /* set default parameters for Secure Simple Pairing */
+    esp_bt_sp_param_t param_type = ESP_BT_SP_IOCAP_MODE;
+    esp_bt_io_cap_t iocap = ESP_BT_IO_CAP_IO;
+    esp_bt_gap_set_security_param(param_type, &iocap, sizeof(uint8_t));
+#endif
+
+    /* set default parameters for Legacy Pairing (use fixed pin code 1234) */
+    esp_bt_pin_type_t pin_type = ESP_BT_PIN_TYPE_FIXED;
+    esp_bt_pin_code_t pin_code;
+    pin_code[0] = '1';
+    pin_code[1] = '2';
+    pin_code[2] = '3';
+    pin_code[3] = '4';
+    esp_bt_gap_set_pin(pin_type, 4, pin_code);
+
+    ESP_LOGI(BT_AV_TAG, "Own address:[%s]", bda2str((uint8_t *)esp_bt_dev_get_address(), bda_str, sizeof(bda_str)));
+    bt_app_task_start_up();
+    /* bluetooth device name, connection mode and profile set up */
+    bt_app_work_dispatch(bt_av_hdl_stack_evt, BT_APP_EVT_STACK_UP, NULL, 0, NULL);
 }
-   
